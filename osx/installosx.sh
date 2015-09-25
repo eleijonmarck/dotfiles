f2b95c2096e558eaedb2790e4990f91326a67aed
eleijonmarck <ericle@kth.se>
2015-09-25 06:06:23 +0200

    Added all keyreapeat commands

--- osx/installosx.sh
+++ osx/installosx.sh
@@ -7,10 +7,12 @@ echo "Disable press-and-hold for keys in favor of key repeat"
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 echo "Set a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write -g KeyRepeat -int 1
 
 echo "Set a shorter Delay until key repeat"
-defaults write NSGlobalDomain InitialKeyRepeat -int 8
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write -g InitialKeyRepeat -int 10
 
 # echo "Finder: show all filename extensions"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
