f7e172d563bf51eb69146000e016eb2b0efc9670
eleijonmarck <ericle@kth.se>
2015-10-15 21:46:21 +0200

    added some shit

--- common/go_install.sh
+++ common/go_install.sh
@@ -5,3 +5,22 @@ mkdir -p $HOME/dev/go/src/github.com/eleijonmarck
 
 go get golang.org/x/tools/cmd/godoc
 go get golang.org/x/tools/cmd/vet
+
+
+# goimports
+go get -u golang.org/x/tools/cmd/goimports
+
+# gocode
+go get -u github.com/nsf/gocode
+
+# godef
+go get -u github.com/rogpeppe/godef
+
+# golint
+go get -u github.com/golang/lint/golint
+
+# errcheck
+go get -u github.com/kisielk/errcheck
+
+# oracle
+go get -u golang.org/x/tools/cmd/oracle
