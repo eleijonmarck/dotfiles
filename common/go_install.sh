a5f00d1c6f59b93a3a03874e55369fd6d01efb10
eleijonmarck <ericle@kth.se>
2015-10-29 22:47:29 +0100

    install goä

--- common/go_install.sh
+++ common/go_install.sh
@@ -1,11 +1,23 @@
 #! /bin/sh 
 
-mkdir -p $HOME/dev/go
-mkdir -p $HOME/dev/go/src/github.com/eleijonmarck
+echo "Please enter your golang path (ex: $HOME/golang) :"
+read gopath
 
-go get golang.org/x/tools/cmd/godoc
+echo "Please enter your github username (ex: vsouza) :"
+read user
+
+mkdir $gopath
+mkdir -p $gopath/src/github.com/$user
+
+export GOPATH=$gopath
+export GOROOT=/usr/local/opt/go/libexec
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOROOT/bin
+
 go get golang.org/x/tools/cmd/vet
 
+#godoc
+go get golang.org/x/tools/cmd/godoc
 
 # goimports
 go get -u golang.org/x/tools/cmd/goimports
@@ -22,5 +34,11 @@ go get -u github.com/golang/lint/golint
 # errcheck
 go get -u github.com/kisielk/errcheck
 
+# gocodeexaminer
+go get golang.org/x/tools/cmd/vet
+
 # oracle
 go get -u golang.org/x/tools/cmd/oracle
+
+echo "go to https://golang.org/doc/code.html and enjoy :D"
+echo "also make sure that you put the exports into .zsh/.profile"
